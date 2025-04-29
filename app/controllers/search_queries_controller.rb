bb30b4e05238a94e45e1d054b22745d3b9c33331
Isabella <isabellaotoo25@gmail.com>
2025-04-29 11:40:23 +0000

    Update SearchQueries Controller

--- app/controllers/search_queries_controller.rb
+++ app/controllers/search_queries_controller.rb
@@ -1,7 +1,19 @@
 class SearchQueriesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def index
+    @top_queries = SearchQuery.group(:query_text).order('count_id DESC').limit(10).count(:id)
   end
 
   def create
+    ip = request.remote_ip
+    query_text = params[:query_text].to_s.strip
+  
+    return redirect_to root_path if query_text.blank?
+  
+    SearchQuery.create(ip_address: ip, query_text: query_text)
+  
+    redirect_to root_path(query: query_text)
   end
+  
 end
